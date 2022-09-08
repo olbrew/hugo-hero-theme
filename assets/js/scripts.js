db44a5c3a13e9af1e26edafde81ab12504e2faad
olbrew <olivierbrewaeys@gmail.com>
2022-09-09 01:05:00 +0200

    Mobile menu now properly hides itself after clicking anchor links in it

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,11 @@
 var body = document.querySelector('body')
-var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+var triggers = document.querySelectorAll('.toggle-mobile-menu');
 
-menuTrigger.onclick = function() {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
+for (var trigger of triggers) {
+    trigger.onclick = function() {
+        menuContainer.classList.toggle('open');
+        menuTrigger.classList.toggle('is-active')
+        body.classList.toggle('lock-scroll')
+    }
 }
